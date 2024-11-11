e88e1341bf9438367d5007209b9d13c88cc3d47d
Neolux Lee <neolux.lee@gmail.com>
2024-11-12 01:45:28 +0800

    完成开机启动脚本

--- service/appli.sh
+++ service/appli.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+me=$(readlink -f $0)
+echo "Running $me"
+
+if [ -L /usr/local/bin/appli.sh ]; then
+    rm /usr/local/bin/appli.sh
+fi
+echo "Linking $me to /usr/local/bin/appli.sh"
+ln -sf $me /usr/local/bin/appli.sh
+
 HOME=/root
-WS=$HOME/dev_ws/gz
+WS=$HOME/dev_ws/appli
 export CAM_TYPE=usb
 
-source /opt/tros/setup.bash
+source /opt/tros/humble/setup.bash
 source $WS/install/setup.bash
 
-ros2 launch gz car.launch.py
+ros2 launch $WS/launch/run_all.launch.py
 
 echo "Done! With ret code $?"
 
